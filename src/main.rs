f86f79c49ff3647e91bf1d9855f1f00af6952f4c
scullionw <scuw1801@usherbrooke.ca>
2018-07-05 18:38:16 -0400

    2way kinda working

--- src/main.rs
+++ src/main.rs
@@ -3,42 +3,101 @@ extern crate notify;
 use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 
 use std::env;
+use std::fs;
+use std::marker::{Send, Sync};
 use std::path::Path;
 use std::process::Command;
+use std::process::Stdio;
 use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+use std::thread;
 use std::time::Duration;
 
-struct Watchdog<T: Reactor> {
+struct Monitor {
     _watcher: RecommendedWatcher,
     rx: mpsc::Receiver<notify::DebouncedEvent>,
-    reactor: T,
-    mode: DiffMode,
 }
 
-impl<T: Reactor> Watchdog<T> {
-    fn new(reactor: T, mode: DiffMode) -> Watchdog<T> {
+impl Monitor {
+    fn new(dir: &str) -> Self {
         let (tx, rx) = mpsc::channel();
         let mut watcher: RecommendedWatcher =
             Watcher::new(tx, Duration::from_secs(2)).expect("Couldn't choose a watcher.");
 
-        watcher
-            .watch(reactor.monitored(), RecursiveMode::Recursive)
-            .unwrap();
+        watcher.watch(dir, RecursiveMode::Recursive).unwrap();
 
-        Watchdog {
+        Self {
             _watcher: watcher,
             rx,
-            reactor,
+        }
+    }
+}
+
+struct Watchdog<T: Reactor + Send + Sync + 'static> {
+    reactor: Arc<Mutex<T>>,
+    mode: DiffMode,
+}
+
+fn useful_notice(event: &notify::DebouncedEvent) -> bool {
+    match event {
+        notify::DebouncedEvent::Remove(_x)
+        | notify::DebouncedEvent::Create(_x)
+        | notify::DebouncedEvent::Write(_x) => true,
+        notify::DebouncedEvent::Rename(_x, _y) => true,
+        _ => false,
+    }
+}
+
+impl<T: Reactor + Send + Sync + 'static> Watchdog<T> {
+    fn new(reactor: T, mode: DiffMode) -> Self {
+        Self {
+            reactor: Arc::new(Mutex::new(reactor)),
             mode,
         }
     }
 
-    fn run(self) -> Result<(), &'static str> {
-        for _ in self.rx {
-            self.reactor.run()?;
-            show_not_copied(self.mode, &self.reactor.monitored(), &self.reactor.target())?;
+    fn run(self) {
+        let source_monitor = Monitor::new(&self.reactor.lock().unwrap().monitored());
+        let target_monitor = Monitor::new(&self.reactor.lock().unwrap().target());
+
+        let c_reactor = Arc::clone(&self.reactor);
+
+        let source_handle = thread::spawn(move || {
+            for (i, event) in source_monitor
+                .rx
+                .iter()
+                .filter(|x| useful_notice(x))
+                .enumerate()
+            {
+                if let Ok(ref lock) = self.reactor.try_lock() {
+                    lock.run().unwrap();
+                    show_not_copied(self.mode, &lock.monitored(), &lock.target()).unwrap();
+                    println!("S -> T = {} : {:?}", i, event)
+                } else {
+                    println!("S -> T = {} LOCKED : {:?}", i, event)
+                }
+            }
+        });
+
+        let target_handle = thread::spawn(move || {
+            for (i, event) in target_monitor
+                .rx
+                .iter()
+                .filter(|x| useful_notice(x))
+                .enumerate()
+            {
+                if let Ok(ref lock) = c_reactor.try_lock() {
+                    lock.run_reverse().unwrap();
+                    println!("T -> S = {} : {:?}", i, event)
+                } else {
+                    println!("T -> S = {} LOCKED : {:?}", i, event)
+                }
+            }
+        });
+
+        for handle in vec![source_handle, target_handle] {
+            handle.join().unwrap();
         }
-        Ok(())
     }
 }
 
@@ -68,26 +127,28 @@ fn main() -> Result<(), &'static str> {
     let dropbox_mirror = Mirror::new(source, target, ignorefile);
     let watchdog = Watchdog::new(dropbox_mirror, DiffMode::NotCopied);
 
-    watchdog.run()?;
+    watchdog.run();
 
     Ok(())
 }
 
+#[derive(Clone)]
 struct Mirror {
     source: String,
     target: String,
     ignorefile: String,
 }
 
-trait Reactor {
+trait Reactor: Clone {
     fn run(&self) -> Result<(), &'static str>;
+    fn run_reverse(&self) -> Result<(), &'static str>;
     fn monitored(&self) -> String;
     fn target(&self) -> String;
 }
 
 impl Mirror {
-    fn new(source: &str, target: &str, ignorefile: &str) -> Mirror {
-        Mirror {
+    fn new(source: &str, target: &str, ignorefile: &str) -> Self {
+        Self {
             source: source.to_owned(),
             target: target.to_owned(),
             ignorefile: ignorefile.to_owned(),
@@ -97,24 +158,11 @@ impl Mirror {
 
 impl Reactor for Mirror {
     fn run(&self) -> Result<(), &'static str> {
-        let output = Command::new("rsync")
-                            .arg("-a")
-                            .arg("--delete")
-                            .arg("--exclude-from")
-                            .arg(&self.ignorefile)
-                            .arg(&self.source) // TODO: add trailing slash
-                            .arg(&self.target)
-                            .output()
-                            .expect("rsync failed to start");
+        rsync(&self.source, &self.target, &self.ignorefile)
+    }
 
-        if !output.status.success() {
-            println!("status: {}", output.status);
-            println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-            eprintln!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-            Err("rsync failed")
-        } else {
-            Ok(())
-        }
+    fn run_reverse(&self) -> Result<(), &'static str> {
+        rsync(&self.target, &self.source, &self.ignorefile)
     }
 
     fn monitored(&self) -> String {
@@ -123,6 +171,27 @@ impl Reactor for Mirror {
 
     fn target(&self) -> String {
         self.target.clone()
+    }
+}
+
+fn rsync(source: &str, target: &str, ignorefile: &str) -> Result<(), &'static str> {
+    let output = Command::new("rsync")
+                            .arg("-a")
+                            .arg("--delete")
+                            .arg("--exclude-from")
+                            .arg(ignorefile)
+                            .arg(source) // TODO: add trailing slash
+                            .arg(target)
+                            .output()
+                            .expect("rsync failed to start");
+
+    if output.status.success() {
+        Ok(())
+    } else {
+        println!("status: {}", output.status);
+        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+        eprintln!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+        Err("rsync failed")
     }
 }
 
@@ -141,10 +210,8 @@ enum DiffMode {
 // https://github.com/oconnor663/duct.rs
 // https://github.com/oconnor663/os_pipe.rs
 
-use std::fs;
-use std::process::Stdio;
-
 // create or append to file
+// deal with two-way logs
 fn show_not_copied(mode: DiffMode, source_dir: &str, target_dir: &str) -> Result<(), &'static str> {
     match mode {
         DiffMode::NotCopied => {
@@ -154,31 +221,21 @@ fn show_not_copied(mode: DiffMode, source_dir: &str, target_dir: &str) -> Result
                 .spawn() // Why not output?
                 .expect("diff failed to run");
 
-            // if !diff_output.status.success() {
-            //     println!("status: {}", diff_output.status);
-            //     println!("stdout: {}", String::from_utf8_lossy(&diff_output.stdout));
-            //     eprintln!("stderr: {}", String::from_utf8_lossy(&diff_output.stderr));
-            //     Err("diff failed")
-            // } else {
-            //     println!("stdout: {}", String::from_utf8_lossy(&diff_output.stdout));
-            //     Ok(())
-            // }
-
             let grep_output = Command::new("grep")
                 .arg(["Only in", source_dir].join(" "))
                 .stdin(diff_output.stdout.unwrap())
                 .output()
                 .expect("grep failed to run");
 
-            if !grep_output.status.success() {
+            if grep_output.status.success() {
+                let result = String::from_utf8_lossy(&grep_output.stdout);
+                fs::write(".rsynclog", &result[..]).unwrap();
+                Ok(())
+            } else {
                 println!("status: {}", grep_output.status);
                 println!("stdout: {}", String::from_utf8_lossy(&grep_output.stdout));
                 eprintln!("stderr: {}", String::from_utf8_lossy(&grep_output.stderr));
                 Err("diff or grep failed")
-            } else {
-                let result = String::from_utf8_lossy(&grep_output.stdout);
-                fs::write(".rsynclog", &result[..]).unwrap();
-                Ok(())
             }
         }
         DiffMode::Off => Ok(()),
